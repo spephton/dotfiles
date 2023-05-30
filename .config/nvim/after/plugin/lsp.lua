acc7f932f34d7fd8ccdbf8b97c6f601e4d1bbe39
Jacob Sephton <71482036+spephton@users.noreply.github.com>
2023-05-31 01:17:52 +1000

    Add notes on formatting

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -17,15 +17,17 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	['<Tab>'] = cmp.mapping.confirm({ select = true }),
 	['<C-Space>'] = cmp.mapping.complete(),
 })
-
 
 
 lsp.setup_nvim_cmp({
 	mapping = cmp_mappings
 })
 
+-- keymap gq for format
+-- gqip (format in paragraph)
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
